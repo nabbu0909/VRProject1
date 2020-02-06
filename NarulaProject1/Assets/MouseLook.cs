8ebf54bde9ea31d8a2fcf45b9513da4cb4685b00
nabbu0909 <navam.narula@gmail.com>
2020-02-05 17:25:37 -0800

    Added mouselook

---
+++ NarulaProject1/Assets/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public float clampAngle;
+    
+    private float rotationX;
+    private float rotationY; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotationY = rot.y;
+        rotationX = rot.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+ 
+        rotationY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotationX += mouseY * mouseSensitivity * Time.deltaTime;
+ 
+        rotationX = Mathf.Clamp(rotationX, -clampAngle, clampAngle);
+ 
+        Quaternion totalRotation = Quaternion.Euler(rotationX, rotationY, 0.0f);
+        transform.rotation = totalRotation;
+    }
+}
