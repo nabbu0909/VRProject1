cc302473d2f7935e84eecefedfda65b5fc8cbe6e
nabbu0909 <navam.narula@gmail.com>
2020-02-08 11:52:30 -0800

    Game loop created

--- NarulaProject1/Assets/GameManager.cs
+++ NarulaProject1/Assets/GameManager.cs
@@ -9,9 +9,12 @@ public class GameManager : MonoBehaviour
     public Button exitText;
     public float speedIncrement;
     public GameObject player;
+    public Goal goal;
+
     public GameObject ProjPrefab;
     public GameObject Proj;
     public Text counter;
+    public Text finalScore;
     public float projSpeed;
     // Start is called before the first frame update
     void Start()
@@ -22,25 +25,32 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKeyDown("e")){
-            projSpeed += speedIncrement;
-        }
-        if(Input.GetKeyDown("q")){
-            projSpeed -= speedIncrement;
-        }
-        if(Input.GetMouseButtonDown(0) && !exitText.IsActive()) {
-            Proj = Instantiate<GameObject>(ProjPrefab);
-            Proj.transform.position = player.transform.position + Camera.main.transform.forward;
-            Rigidbody rb = Proj.GetComponent<Rigidbody>();
-            rb.velocity = Camera.main.transform.forward * projSpeed;
-        }
+    {   
+        if (Time.timeSinceLevelLoad < 60){
+            if(Input.GetKeyDown("e")){
+                projSpeed += speedIncrement;
+            }
+            if(Input.GetKeyDown("q")){
+                projSpeed -= speedIncrement;
+            }
+            if(Input.GetMouseButtonDown(0) && !exitText.IsActive()) {
+                Proj = Instantiate<GameObject>(ProjPrefab);
+                Proj.transform.position = player.transform.position + Camera.main.transform.forward;
+                Rigidbody rb = Proj.GetComponent<Rigidbody>();
+                rb.velocity = Camera.main.transform.forward * projSpeed;
+            }
 
-        if(Input.GetKeyDown(KeyCode.Escape) && exitText.IsActive()) {
-            Cursor.lockState = CursorLockMode.Locked;
-            exitText.gameObject.SetActive(false);
+            if(Input.GetKeyDown(KeyCode.Escape) && exitText.IsActive()) {
+                Cursor.lockState = CursorLockMode.Locked;
+                exitText.gameObject.SetActive(false);
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape)) {
+                Cursor.lockState = CursorLockMode.Confined;
+                exitText.gameObject.SetActive(true);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Escape)) {
+        else {
+            finalScore.text = "Score: " + goal.pointsMade;
             Cursor.lockState = CursorLockMode.Confined;
             exitText.gameObject.SetActive(true);
         }
