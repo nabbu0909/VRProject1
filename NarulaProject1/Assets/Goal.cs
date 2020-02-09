195065f16798b60093a223166e3d2dfaf08eb408
Navam Narula <navam.narula@gmail.com>
2020-02-09 16:49:55 -0600

    Added Sounds

--- NarulaProject1/Assets/Goal.cs
+++ NarulaProject1/Assets/Goal.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
+    public AudioClip pointSound;
     public Transform goal;
     public Text score;
     public float pointsMade = 0;
@@ -22,6 +23,7 @@ public class Goal : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         pointsMade++;
+        AudioSource.PlayClipAtPoint(pointSound, transform.position);
         Destroy(other.gameObject);
         goal.position = new Vector3(Random.Range(-50.0f, 50.0f), -0.3f, Random.Range(-50.0f, 50.0f));
     }
