340d0d5dd46f7258fe5c723f5c59fca856440469
Navam Narula <navam.narula@gmail.com>
2020-02-09 16:10:42 -0600

    Added Menu Functionality

---
+++ NarulaProject1/Assets/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void beginGame()
+    {
+        Debug.Log("Begin Game");
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+    public void quitGame()
+    {
+        Debug.Log("End Game");
+        Application.Quit();
+    }
+}
